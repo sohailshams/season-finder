dee9ba728a7ed830ef4d3b3ed589d0bf3ef4e671
sohailshams <sohailshamas@yahoo.com>
2021-01-01 18:35:05 +0000

    Updated SeasonDisplay component - added if statement to show if it is winter or summer

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,7 +1,25 @@
 import React from 'react';
 
-const SeasonDisplay = () => {
-  return <div>Season Display</div>;
+const getSeason = (lat, month) => {
+  if (month > 2 && month < 9) {
+    return lat > 0 ? 'Summer' : 'Winter';
+  } else {
+    return lat > 0 ? 'Winter' : 'Summer';
+  }
+};
+
+const SeasonDisplay = (props) => {
+  const season = getSeason(props.lat, new Date().getMonth());
+  const text =
+    season === 'Winter' ? 'Ahhhhhh, it is freaking cold' : 'It is nice sunny';
+  const icon = season === 'Winter' ? 'snowflake' : 'sun';
+  return (
+    <div>
+      <i className={`${icon} icon`} />
+      <h1>{text}</h1>
+      <i className={`${icon} icon`} />
+    </div>
+  );
 };
 
 export default SeasonDisplay;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay';
+import 'semantic-ui-css/semantic.min.css';
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = { lat: null, errorMessage: '' };
-  }
+  state = { lat: null, errorMessage: '' };
 
   componentDidMount() {
     window.navigator.geolocation.getCurrentPosition(
@@ -21,7 +19,7 @@ class App extends React.Component {
     }
 
     if (!this.state.errorMessage && this.state.lat) {
-      return <div>latitude: {this.state.lat}</div>;
+      return <SeasonDisplay lat={this.state.lat} />;
     }
 
     return <div>Loading</div>;
